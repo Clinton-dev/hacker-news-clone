bbc28863a7f2c1a3bbeec67f4210ed6ba252eb85
Clinton-dev <clintonwambugu@gmail.com>
2023-07-12 15:46:24 +0300

    Check for click functionality in favorites page

--- pages/favorites.js
+++ pages/favorites.js
@@ -1,6 +1,7 @@
 import view from "../utils/view.js";
 import store from "../store.js";
 import Story from "../components/Story.js";
+import checkFavorites from "../utils/checkFavorites.js";
 
 export default function Favorites() {
   const { favorites } = store.getState();
@@ -9,8 +10,24 @@ export default function Favorites() {
   view.innerHTML = `<div>
     ${
       hasFavorite
-        ? favorites.map((favorite) => Story(favorite)).join("")
-        : "<div class='center-container'><img id='unhappy-icon' src='../images/unhappy_crying_icon.png'/><p>You have no News favorites<p></div>"
+        ? favorites
+            .map((story) =>
+              Story({ ...story, isFavorite: checkFavorites(favorites, story) })
+            )
+            .join("")
+        : "<div class='center-container'><img id='unhappy-icon' src='../images/unhappy_crying_icon.png'/><p>You have no News favorites, plz add some!!!<p></div>"
     }
   </div>`;
+
+  document.querySelectorAll(".favorite").forEach((favoriteButton) => {
+    favoriteButton.addEventListener("click", function () {
+      const story = JSON.parse(this.dataset.story);
+      const isFavorited = checkFavorites(favorites, story);
+      store.dispatch({
+        type: isFavorited ? "REMOVE_FAVORITE" : "ADD_FAVORITE",
+        payload: { favorite: story },
+      });
+      Favorites();
+    });
+  });
 }
