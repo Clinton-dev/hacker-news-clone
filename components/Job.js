382f7f914aa53e4eeb746421552fb9f9f272dacf
Clinton-dev <clintonwambugu@gmail.com>
2023-07-12 17:30:37 +0300

    update job styles

--- components/Job.js
+++ components/Job.js
@@ -1,12 +1,12 @@
 export default function Job(job) {
   // Display the first 10 jobs then user should press a button to show more jobs
-  
+
   const { title, domain, url, time_ago } = job;
 
   return `<div>
             <div>
                 <a href="${url}">${title}</a>
-                <span>(${domain})</span>
+                <span class="gray">(${domain})</span>
             </div>
         <div class="job-detail">
             <div class="gray">
