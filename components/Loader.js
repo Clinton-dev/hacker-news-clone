27abc1201b49100b8f2cf5da974be8f59789685d
Clinton-dev <clintonwambugu@gmail.com>
2023-07-13 07:56:38 +0300

    Add loader component

---
+++ components/Loader.js
@@ -0,0 +1,8 @@
+export default function Loader() {
+  return `<div id="loader">
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+        <div class="dot"></div>
+ </div>`;
+}
